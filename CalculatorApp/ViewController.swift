20b1205fc1b293f6a0a52055fd1295047c78efaf
SMizzz <shinn.mizzz@gmail.com>
2022-10-16 20:37:22 +0900

    사칙 연산 우선 순위 버그 수정

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -15,59 +15,64 @@ enum CalculationOperator: String {
 }
 
 struct Operation {
-    var index: [Int] = []
-    var resultString = ""
-    var num1: Double = 0.0
-    var num2: Double = 0.0
-    var result = 0.0
-    var operate: [String] = ["x", "/", "+", "-"]
+    var index: [Int] = [] // 연산자 기준으로 왼쪽과 오른쪽을 계산할 예정이므로 필요한 인덱스 위치를 저장하는 변수 배열
+    var result: Double = 0.0 // Double타입의 결과를 저장할 변수
+    var resultString = "" // Double타입 -> String타입으로 변환할 변수
+    var operate: [String] = ["x", "÷", "+", "-"] // stringArray 배열에 있는 연산자와 비교해 계산하기 위한 연산자배열
     
     mutating func performOperation(_ stringArray: [String]) -> Double {
+        // 계산하고 불필요한 요소들을 삭제하기 위해 상수인 stringArray를 변수에 저장
         var arr = stringArray
-        for oper in operate {
-          for i in 0...arr.count - 1 {
-            if oper == arr[i] {
-              switch oper {
-              case CalculationOperator.multiply.rawValue:
-                  result = Double(arr[i - 1])! * Double(arr[i + 1])!
-              case CalculationOperator.divide.rawValue:
-                  result = Double(arr[i - 1])! / Double(arr[i + 1])!
-              case CalculationOperator.plus.rawValue:
-                  result = Double(arr[i - 1])! + Double(arr[i + 1])!
-              case CalculationOperator.minus.rawValue:
-                  result = Double(arr[i - 1])! - Double(arr[i + 1])!
-                default:
-                  break
-              }
-              resultString = String(result)
-              arr[i] = resultString
-              index.append(i)
+        
+        while(arr.count > 1) {
+            
+            var oper = CalculationOperator.multiply
+            
+            if arr.contains("x") {
+                oper = CalculationOperator.multiply
+            } else if arr.contains("/") {
+                oper = CalculationOperator.divide
+            } else if arr.contains("+") {
+                oper = CalculationOperator.plus
+            } else if arr.contains("-") {
+                oper = CalculationOperator.minus
             }
-          }
-
-          index.sort(by: > )
-          for i in index {
-            arr.remove(at: i + 1)
-            arr.remove(at: i - 1)
-          }
-          index.removeAll()
+            
+            for i in 0...arr.count - 1 {
+                if oper.rawValue == arr[i] { // 연산자 비교해서 계산
+                    switch oper {
+                    case CalculationOperator.multiply:
+                        result = (Double(arr[i - 1]) ?? 0.0) * (Double(arr[i + 1]) ?? 0.0)
+                    case CalculationOperator.divide:
+                        result = (Double(arr[i - 1]) ?? 0.0) / (Double(arr[i + 1]) ?? 0.0)
+                    case CalculationOperator.plus:
+                        result = (Double(arr[i - 1]) ?? 0.0) + (Double(arr[i + 1]) ?? 0.0)
+                    case CalculationOperator.minus:
+                        result = (Double(arr[i - 1]) ?? 0.0) - (Double(arr[i + 1]) ?? 0.0)
+                    }
+                    
+                    arr[i] = String(result)
+                    arr.remove(at: i + 1)
+                    arr.remove(at: i - 1)
+                    
+                    break
+                }
+            }
         }
+        
         return Double(arr[0]) ?? 0.0
     }
 }
 
 class ViewController: UIViewController {
     var operation = Operation()
-    var displayNumberString = ""
-    var numArray: [Double] = []
+    var pressValueByUser: String = "" // numStringArray 배열에 넣을 데이터를 저장하는 변수
+    var insertLabelString: String = "" // numLabel에 보여질 변수
     var numStringArray: [String] = []
-    var operatorKey: String = ""
-    var operatorArray: [String] = []
-    
-    var pressOperationBtnbyUser: String = ""
     var result = 0.0
     
     @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -75,38 +80,63 @@ class ViewController: UIViewController {
     
     @IBAction func numberBtnTapped(_ sender: UIButton) {
         guard let numberValue = sender.titleLabel?.text else { return }
-        displayNumberString += numberValue
-        numberLabel.text = displayNumberString
+        pressValueByUser += numberValue
+        insertLabelString += numberValue
+        numberLabel.textColor = .black
+        numberLabel.text = insertLabelString
     }
     
     @IBAction func pressAllClearButton(_ sender: Any) {
-        displayNumberString = ""
-        numberLabel.text = displayNumberString
+        pressValueByUser = ""
+        insertLabelString = ""
+        numberLabel.text = insertLabelString
+        resultLabel.text = ""
         numStringArray.removeAll()
     }
     
+    @IBAction func pressDotButton(_ sender: Any) {
+        
+    }
+    
     @IBAction func pressZeroButton(_ sender: Any) {
-        displayNumberString += "0"
+        
     }
     
     @IBAction func pressOperationButton(_ sender: UIButton) {
-        numStringArray.append(displayNumberString)
+        numStringArray.append(pressValueByUser)
         guard let operationText = sender.titleLabel?.text else { return }
+        pressValueByUser += operationText
+        insertLabelString += operationText
         numStringArray.append(operationText)
-        displayNumberString = ""
-        numberLabel.text = ""
+        
+        pressValueByUser = ""
+        numberLabel.text = insertLabelString
     }
     
     @IBAction func pressResultButton(_ sender: Any) {
-        numStringArray.append(displayNumberString)
-        result = operation.performOperation(numStringArray)
+        numStringArray.append(pressValueByUser)
         
+        // 만약 사용자가 공백을 넣었을 경우
+        for i in 0...numStringArray.count - 1 {
+            if numStringArray[i] == "" {
+                numStringArray[i] = "0"
+            }
+        }
+        
+        result = operation.performOperation(numStringArray)
         if result.isInfinite {
             result = 0.0
         }
         
-        numberLabel.text = "\(result)"
-        displayNumberString = ""
+        if String(result).hasSuffix(".0") {
+            resultLabel.text = "\(Int(result))"
+        } else {
+            resultLabel.text = "\(result)"
+        }
+        
+        numberLabel.textColor = .lightGray
+        pressValueByUser = ""
+        insertLabelString = ""
         numStringArray.removeAll()
         result = 0
     }
